e5016d5e180c5ab8e2296b164bc28408144429d5
aneto <aneto@CELTIC.voiza.com.br>
2013-03-12 11:56:22 -0300

    Ajuste no utilitário de datas para usar JodaTime e Holiday Manager

--- src/main/java/br/com/voiza/rse/util/DateUtil.java
+++ src/main/java/br/com/voiza/rse/util/DateUtil.java
@@ -1,51 +1,116 @@
 package br.com.voiza.rse.util;
 
+import de.jollyday.Holiday;
+import de.jollyday.HolidayCalendar;
+import de.jollyday.HolidayManager;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import net.objectlab.kit.datecalc.common.DateCalculator;
+import net.objectlab.kit.datecalc.common.DefaultHolidayCalendar;
+import net.objectlab.kit.datecalc.common.HolidayHandlerType;
+import net.objectlab.kit.datecalc.joda.LocalDateKitCalculatorsFactory;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
+import org.joda.time.LocalDate;
+import org.joda.time.chrono.ISOChronology;
 
 /**
  * Classe utilitária para tratar situações com datas
- * 
+ *
  * @author aneto
  */
 public class DateUtil {
-    
+
+    public static final SimpleDateFormat DIA_MES = new SimpleDateFormat("dd/MM");
+
     /**
      * Calcula a diferença em dias entre duas datas.
+     *
      * @param initial Data inicial de onde se deseja obter a diferença
      * @param end Data final de onde se deseja obter a diferença
      * @return a diferença em dias
      */
     public static Integer differenceInDays(Date initial, Date end) {
-        return Days.daysBetween(new DateTime(initial), new DateTime(end)).getDays();
+        return Days.daysBetween(new DateTime(initial).toDateMidnight(), new DateTime(end).toDateMidnight()).getDays();
+    }
+
+    /**
+     * Retorna a quantidade de dias úteis presentes no período informado. Serão
+     * descontados feriados e finais de semana.
+     *
+     * @param initial
+     * @param end
+     * @return
+     */
+    public static Integer businessDaysInPeriod(Date initial, Date end) {
+        LocalDate dataInicialTemporaria = new LocalDate(initial);
+        LocalDate dataFinalTemporaria = new LocalDate(end);
+
+        Integer diasNaoUteis = 0;
+        while (!dataInicialTemporaria.isAfter(dataFinalTemporaria)) {
+            if (isBusinessDay(dataInicialTemporaria)) {
+                diasNaoUteis++;
+            }
+            dataInicialTemporaria = dataInicialTemporaria.plusDays(1);
+        }
+        
+        int dias = differenceInDays(initial, end);
+        
+        return dias - diasNaoUteis;
+    }
+
+    /**
+     * Verifica se um dia é útil
+     * Serão considerados como não útil: Sábados, Domingos e Feriados
+     * @param data Date
+     * @return 
+     */
+    public static boolean isBusinessDay(Calendar data) {
+        return isBusinessDay(new LocalDate(data));
+    }    
+    
+    /**
+     * Verifica se um dia é útil
+     * Serão considerados como não útil: Sábados, Domingos e Feriados
+     * @param data Date
+     * @return 
+     */
+    public static boolean isBusinessDay(Date data) {
+        return isBusinessDay(new LocalDate(data));
     }
     
     /**
-     * Define se uma data cai em um fim de semana.
-     * @param data A data com o dia que se deseja saber se é fim de semana ou não
-     * @return boolean informando se é fim de semana
+     * Verifica se um dia é útil
+     * Serão considerados como não útil: Sábados, Domingos e Feriados
+     * @param data LocalDate
+     * @return 
      */
-    public static boolean isWeekend(Calendar data) {
-        return data.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || data.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY;
+    public static boolean isBusinessDay(LocalDate data) {
+        DefaultHolidayCalendar<LocalDate> holidays = new DefaultHolidayCalendar<LocalDate>(getHolidaysList());
+        LocalDateKitCalculatorsFactory.getDefaultInstance().registerHolidays("BR", holidays);
+        DateCalculator calendar = LocalDateKitCalculatorsFactory.getDefaultInstance().getDateCalculator("BR", HolidayHandlerType.FORWARD);
+
+        return calendar.isNonWorkingDay(data);
     }
-    
-    public static void main(String[] args) {
-        Calendar data1 = Calendar.getInstance();
-        data1.set(2013, 1, 25);
-        data1.set(Calendar.HOUR, 0);
-        data1.set(Calendar.MINUTE, 0);
-        data1.set(Calendar.SECOND, 0);
-        data1.set(Calendar.MILLISECOND, 0);
-        Calendar data2 = Calendar.getInstance();
-        data2.set(2013, 2, 2);
-        data2.set(Calendar.HOUR, 0);
-        data2.set(Calendar.MINUTE, 0);
-        data2.set(Calendar.SECOND, 0);
-        data2.set(Calendar.MILLISECOND, 0);
-        
-        System.out.println("## " + differenceInDays(data1.getTime(), data2.getTime()));
+
+    /**
+     * Retorna a lista de feriados presentes para o Brasil no ano atual
+     *
+     * @return
+     */
+    private static Set<LocalDate> getHolidaysList() {
+        HolidayManager holidayManager = HolidayManager.getInstance(HolidayCalendar.BRAZIL);
+
+        Set<Holiday> holidays = holidayManager.getHolidays(new DateTime().getYear());
+        Set<LocalDate> holidaysList = new HashSet<LocalDate>();
+        for (Holiday holiday : holidays) {
+            holidaysList.add(new LocalDate(holiday.getDate(), ISOChronology.getInstance()));
+        }
+
+        return holidaysList;
     }
-    
+
 }
